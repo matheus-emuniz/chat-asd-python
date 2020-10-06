8d13e8b7e0cf8e98a3aa92776a577fc1d1038feb
Arthur Passos <arthur.pcorrea@sempreceub.com>
2020-10-05 23:22:47 -0300

    Implementando envio de mensagens

--- client.py
+++ client.py
@@ -1,58 +1,30 @@
 import socket
-import sys
 import pickle
+import select
+import sys
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 IP = "127.0.0.1"
 PORT = 8080
 
-
-nickname = input("Digite seu nickname: ")
-
-if len(nickname) > 0:
-    server.connect((IP, PORT))
+server.connect((IP, PORT))
 
 print("COMANDOS:\n MSG: -> Enviar mensagem \n PRIVATE:<nick> -> Enviar mensagem privada \n NICK:<novo-nick> -> Trocar de nickame \n SNDFILE:<nick>:<nome_arquivo> -> Envia arquivo \n RECFILE:<nick>:<ip>:<port>:<nome_arquivo> -> Recebe arquivo")
 
 while True:
-    for sock in [sys.stdin, server]:
+    sockets_list = [sys.stdin, server]
+    read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
+    for sock in read_sockets:
         if sock == server:
             message = sock.recv(2048)
+            message = pickle.loads(message)
+            
             print(message)
         else:
             message = input("> ")
-            mensagem_desmembrada = message.split(":")
-            tamanho_mensagem = len(mensagem_desmembrada)
-            command = mensagem_desmembrada[0]
-            if tamanho_mensagem == 3:   #SNDFILE
-                destinatario = mensagem_desmembrada[1].lstrip()
-                message = mensagem_desmembrada[2].lstrip()
-            elif tamanho_mensagem > 3:  #RECFILE
-                destinatario = mensagem_desmembrada[1].lstrip()
-                ip = mensagem_desmembrada[2].lstrip()
-                port = mensagem_desmembrada[3].lstrip()
-                arquivo = mensagem_desmembrada[4].lstrip()
-            else:
-                message = mensagem_desmembrada[1].lstrip()
-        if command == "MSG":
+            if message == "EXIT":
+                server.close()
+                exit()
             server.send(pickle.dumps(message))
-            print(f"{nickname}: {message}")
-        elif command == "PRIVATE":
-            pass
-        elif command == "NICK":
-            nickname = message
-            print(f"Seu novo nickname é: {message}")
-        elif command == "SNDFILE":
-            pass
-        elif command == "RECFILE":
-            flname = 'enviado.png'
-            fyle = open(flname,'wb')
-            print('Iniciando...')
-            fyle.write(server.recv(6053))
-            fyle.close()
-            print('Arquivo recebido!')
-        else:
-            print("Comando inexistente")
-
             
